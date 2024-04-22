7a5e8776614fa5f61b84e33e655fb9fd2800b260
Kalle Vigren <kcrwigren@gmail.com>
2024-04-22 14:49:34 +0200

    actually got the program to work now

--- the_book/c2/guessing_game/src/main.rs
+++ the_book/c2/guessing_game/src/main.rs
@@ -5,23 +5,22 @@ use rand::*;
 fn main() {
     print!("Guess what the secret number is ");
     print!("\n");
-
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-
-                
-    loop {}
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too large"),
-        Ordering::Equal => println!("You win!"),
-    }
-
     
+    let secret: i32 = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Error reading line from input");
+        let guess: i32 = guess.trim().parse().expect("Please type a number!");
+        println!("Your guess: {guess}");
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too large"),
+            Ordering::Equal =>  {
+                println!("Perfect!");
+                break;
+            }
+        }
+    }
 }
